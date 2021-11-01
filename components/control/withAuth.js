a3e030ad0be4b5ff0d08f1b4b49082038d4f149e
Marcin155399 <marcin155399@gmail.com>
2021-11-01 18:41:48 +0100

    stillwithauth

--- components/control/withAuth.js
+++ components/control/withAuth.js
@@ -5,7 +5,7 @@ import { getDoc, doc } from "firebase/firestore";
 import { db } from "./initFirebase";
 import { useEffect } from "react";
 
-//eslint-disable-next-line react/display-nameaaaaa
+//eslint-disable-next-line react/display-name
 function withAuth(WrappedComponent) {
   return (props) => {
     if (typeof window !== "undefined") {
